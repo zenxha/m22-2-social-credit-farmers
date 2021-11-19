c1e932924db4ffc95839876cca1fe4b616ed61b1
zenxha <komays5042@gmail.com>
2021-11-19 10:05:22 -0800

    Add blank template

--- src/main/java/com/social_credit_farming/controllers/MainController.java
+++ src/main/java/com/social_credit_farming/controllers/MainController.java
@@ -17,4 +17,11 @@ public class MainController {
         return "homesite/index"; // returns HTML VIEW (greeting)
 
     }
+    @GetMapping("/blank")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String blank(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
+        model.addAttribute("name", name); // MODEL is passed to html
+        return "base/blank"; // returns HTML VIEW (greeting)
+
+    }
 }
