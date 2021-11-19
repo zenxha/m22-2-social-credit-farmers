3bcbe976b4f4b3895648897cbf92aa394a8005f9
Allen <purplebears321@gmail.com>
2021-11-19 10:11:01 -0800

    quizes

--- src/main/java/com/social_credit_farming/controllers/QuizController.java
+++ src/main/java/com/social_credit_farming/controllers/QuizController.java
@@ -9,14 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
-public class MainController {
-    @GetMapping("/")
+public class QuizController {
+    @GetMapping("/quiz")
     // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+    public String quiz(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
         model.addAttribute("name", name); // MODEL is passed to html
-        return "homesite/index"; // returns HTML VIEW (greeting)
-
+        return "homesite/quiz"; // returns HTML VIEW (greeting)
     }
-
 }
