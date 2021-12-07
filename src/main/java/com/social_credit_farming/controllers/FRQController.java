92e40b1c4ba0eb3988e1a8094d22b1ecb60c4eb4
Allen Xu <allenyxu@users.noreply.github.com>
2021-12-06 17:13:15 -0800

    Fixed CB

--- src/main/java/com/social_credit_farming/controllers/FRQController.java
+++ src/main/java/com/social_credit_farming/controllers/FRQController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AllenController {
-    @GetMapping("/allen")
-    public String About() {
-        return "minilabs/allen/minilabhome";
+public class FRQController {
+    @GetMapping("/cb")
+    public String Frq() {
+        return "minilabs/minilabhome";
     }
 }
