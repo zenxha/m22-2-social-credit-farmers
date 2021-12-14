b38080370f8f2b29e3f16cddb268cb61d3d1d892
Allen <purplebears321@gmail.com>
2021-12-14 10:26:56 -0800

    controller

---
+++ src/main/java/com/social_credit_farming/controllers/CanvasController.java
@@ -0,0 +1,13 @@
+package com.social_credit_farming.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class CanvasController {
+    @GetMapping("/cb/canvas")
+    public String Canvas(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "minilabs/canvas";
+    }
+}
